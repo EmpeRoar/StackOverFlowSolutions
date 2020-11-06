17456e31e31c0f3e83db458ba40a8cb10ce5953f
Julius Bacosa <juliusbacosa@gmail.com>
2020-11-06 12:44:21 +0800

    passing data

---
+++ SO-Solutions/SO.Mvc31/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SO.Mvc31.Controllers
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class EmployeeController : Controller
+    {
+        public EmployeeController()
+        {
+
+        }
+
+        public async Task<IActionResult>  Login()
+        {
+            var emp = new Employee()
+            {
+                ID = 1,
+                Name = "Vladimir Bacosa"
+            };
+            await EmpInput(emp);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EmpInput(Employee emp)
+        {
+            Console.WriteLine(emp.Name);
+            return View();
+        }
+    }
+}
