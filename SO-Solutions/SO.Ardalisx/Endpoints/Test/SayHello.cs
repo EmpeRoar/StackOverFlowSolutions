10e24a91eb99b395d4503ddcefbc0fa285ed2a2e
Julius Bacosa <juliusbacosa@gmail.com>
2020-11-10 00:25:07 +0800

    ardalis endpoint

---
+++ SO-Solutions/SO.Ardalisx/Endpoints/Test/SayHello.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SO.Ardalisx.Endpoints.Test
+{
+    public class SayHello : BaseAsyncEndpoint<string>
+    {
+        public SayHello()
+        {
+        }
+
+        [HttpGet]
+        [Route("Say")]
+        public override async Task<ActionResult<string>> HandleAsync(CancellationToken cancellationToken = default)
+        {
+            return Ok("Nayshhh");
+        }
+    }
+}
